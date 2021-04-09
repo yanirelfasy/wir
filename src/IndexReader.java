84b46516084df215e31b908d253cba991cf05309
Yanir Elfassy <yanir.elfassy@mail.huji.ac.il>
2021-04-09 17:03:28 +0300

    ex1 files

---
+++ src/IndexReader.java
@@ -0,0 +1,82 @@
+package webdata;
+
+public class IndexReader {
+
+	/**
+	* Creates an IndexReader which will read from the given directory
+	*/
+	public IndexReader(String dir) {}
+	
+	/**
+	* Returns the product identifier for the given review
+	* Returns null if there is no review with the given identifier
+	*/
+	public String getProductId(int reviewId) {}
+
+	/**
+	* Returns the score for a given review
+	* Returns -1 if there is no review with the given identifier
+	*/
+	public int getReviewScore(int reviewId) {}
+
+	/**
+	* Returns the numerator for the helpfulness of a given review
+	* Returns -1 if there is no review with the given identifier
+	*/
+	public int getReviewHelpfulnessNumerator(int reviewId) {}
+
+	/**
+	* Returns the denominator for the helpfulness of a given review
+	* Returns -1 if there is no review with the given identifier
+	*/
+	public int getReviewHelpfulnessDenominator(int reviewId) {}
+
+	/**
+	* Returns the number of tokens in a given review
+	* Returns -1 if there is no review with the given identifier
+	*/
+	public int getReviewLength(int reviewId) {}
+
+	/**
+	* Return the number of reviews containing a given token (i.e., word)
+	* Returns 0 if there are no reviews containing this token
+	*/
+	public int getTokenFrequency(String token) {}
+
+	/**
+	* Return the number of times that a given token (i.e., word) appears in
+	* the reviews indexed
+	* Returns 0 if there are no reviews containing this token
+	*/
+	public int getTokenCollectionFrequency(String token) {}
+
+	/**
+	* Return a series of integers of the form id-1, freq-1, id-2, freq-2, ... such
+	* that id-n is the n-th review containing the given token and freq-n is the
+	* number of times that the token appears in review id-n
+	* Only return ids of reviews that include the token
+	* Note that the integers should be sorted by id
+	*
+	* Returns an empty Enumeration if there are no reviews containing this token
+	* /
+	public Enumeration<Integer> getReviewsWithToken(String token) {}
+
+	/**
+	* Return the number of product reviews available in the system
+	*/
+	public int getNumberOfReviews() {}
+
+	/**
+	* Return the number of number of tokens in the system
+	* (Tokens should be counted as many times as they appear)
+	*/
+	public int getTokenSizeOfReviews() {}
+	
+	/**
+	* Return the ids of the reviews for a given product identifier
+	* Note that the integers returned should be sorted by id
+	*
+	* Returns an empty Enumeration if there are no reviews for this product
+	*/
+	public Enumeration<Integer> getProductReviews(String productId) {}
+}
---
+++ src/SlowIndexWriter.java
@@ -0,0 +1,17 @@
+package webdata;
+
+public class SlowIndexWriter {
+
+	/**
+	* Given product review data, creates an on disk index
+	* inputFile is the path to the file containing the review data
+	* dir is the directory in which all index files will be created
+	* if the directory does not exist, it should be created
+	*/
+	public void slowWrite(String inputFile, String dir) {}
+
+	/**
+	* Delete all index files by removing the given directory
+	*/
+	public void removeIndex(String dir) {}
+}
