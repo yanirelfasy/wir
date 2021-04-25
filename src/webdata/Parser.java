9eb6daff57be1b94f67a4240d9a1149fc913fa7c
Yanir Elfassy <yanir.elfassy@mail.huji.ac.il>
2021-04-25 13:30:25 +0300

    Prase given review file

--- src/webdata/Parser.java
+++ src/webdata/Parser.java
@@ -1,2 +1,76 @@
-package webdata;public class Parser {
+package webdata;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.TreeMap;
+
+/**
+ * Class for parsing a file containing reviews
+ */
+public class Parser {
+
+    private ArrayList<Review> reviews = new ArrayList<>();
+    boolean isReadingText = false;
+
+    public void parse(String inputPath, String outputPath){
+        try(BufferedReader reader = new BufferedReader(new FileReader(new File(inputPath)))){
+            String line = reader.readLine();
+            Review review = new Review();
+            while(line != null){
+                String value = "";
+                // Get the product ID
+                if(!(value = this.getFieldContent(line, "product/productId")).equals("-1")){
+                    if(review.isReviewFull()){
+                        reviews.add(review);
+                    }
+                    this.isReadingText = false;
+                    review = new Review();
+                    review.setProductID(value);
+                }
+                // Get the helpfulness
+                else if(!(value = this.getFieldContent(line, "review/helpfulness")).equals("-1")){
+                    review.setHelpfulness(value);
+                }
+                // Get score
+                else if(!(value = this.getFieldContent(line, "review/score")).equals("-1")){
+                    review.setScore(value);
+                }
+                // Mark as reading text
+                else if(!(value = this.getFieldContent(line, "review/text")).equals("-1")){
+                    this.isReadingText = true;
+                    review.addText(value);
+                }
+                else if(isReadingText){
+                    review.addText(line);
+                }
+                line = reader.readLine();
+            }
+            if(review.isReviewFull()){
+                reviews.add(review);
+            }
+            TokensDictionary test = new TokensDictionary(reviews, outputPath);
+            System.out.println("DONE");
+        }
+        catch(IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private String getFieldContent(String line, String fieldTitle){
+        String regexCommand = String.format("^%s: (.*)", fieldTitle);
+        Matcher regexWorker = Pattern.compile(regexCommand).matcher(line);
+        String result = "-1";
+        if(regexWorker.find()){
+            result = regexWorker.group(1);
+        }
+        return result;
+    }
+
+
+
 }
--- src/webdata/Review.java
+++ src/webdata/Review.java
@@ -1,2 +1,69 @@
-package webdata;public class Review {
+package webdata;
+
+import java.util.TreeMap;
+
+public class Review {
+
+    private String productID;
+    private int helpfulnessFirst;
+    private int helpfulnessSecond;
+    private int score;
+    private TreeMap<String, Integer> tokens;
+
+    public Review(){
+        this.tokens = new TreeMap<>();
+        this.helpfulnessFirst = -1;
+        this.helpfulnessSecond = -1;
+        this.score = -1;
+    }
+
+    public void setProductID(String productID){
+        this.productID = productID;
+    }
+
+    public void setHelpfulness(String helpfulness){
+        String [] integers = helpfulness.split("/");
+        if(integers.length == 2){
+            this.helpfulnessFirst = Integer.parseInt(integers[0]);
+            this.helpfulnessSecond = Integer.parseInt(integers[1]);
+        }
+    }
+
+    public void setScore(String score){
+        try{
+            this.score = Integer.parseInt(score.substring(0, score.indexOf('.')));
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void addText(String text){
+        String [] tokens = text.replaceAll("<[^\\>]*>", "").split("[^A-Za-z0-9]");
+        for(String token : tokens){
+            if(!token.equals("")){
+                String lowerToken = token.toLowerCase();
+                if(!this.tokens.containsKey(lowerToken)){
+                    this.tokens.put(lowerToken, 1);
+                }
+                else{
+                    this.tokens.put(lowerToken, this.tokens.get(lowerToken) + 1);
+                }
+            }
+        }
+    }
+
+    public boolean isReviewFull(){
+        return (
+                this.productID != null &&
+                this.score >= 0 &&
+                this.helpfulnessFirst >= 0 &&
+                this.helpfulnessSecond >= 0 &&
+                !this.tokens.isEmpty()
+        );
+    }
+
+    public TreeMap<String, Integer> getTokens(){
+        return this.tokens;
+    }
 }
