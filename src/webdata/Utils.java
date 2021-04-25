f9af421f8e2f2e07a20b550d35d028101f4a30f2
Yanir Elfassy <yanir.elfassy@mail.huji.ac.il>
2021-04-25 13:29:34 +0300

    Utility to write bin file

--- src/webdata/Utils.java
+++ src/webdata/Utils.java
@@ -1,2 +1,23 @@
-package webdata;public class Utils {
+package webdata;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
+public class Utils {
+
+    public static long writeToFile(ArrayList<Byte> encodedData, String output){
+        long positionInFile = -1;
+        try (RandomAccessFile resultFile = new RandomAccessFile(output, "rw")){
+            resultFile.seek(resultFile.length());
+            positionInFile = resultFile.getFilePointer();
+            for (byte encodedByte : encodedData) {
+                resultFile.writeByte(encodedByte);
+            }
+            return positionInFile;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            return positionInFile;
+        }
+    }
 }
